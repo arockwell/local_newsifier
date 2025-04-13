8f9f6a3cd2a29e312675fba50e1068211099ccea
Alex Rockwell <arockwell@gmail.com>
2025-04-12 23:39:06 -0400

    Clean up test files by removing trend-related code

---
+++ tests/performance_test.py
@@ -0,0 +1,16 @@
+"""Script to test the performance of the system."""
+
+import pytest
+from local_newsifier.flows.entity_tracking_flow import EntityTrackingFlow
+
+@pytest.mark.skip(reason="Performance tests not implemented yet")
+def test_large_dataset_processing():
+    pass
+
+@pytest.mark.skip(reason="Performance tests not implemented yet")
+def test_concurrent_processing():
+    pass
+
+@pytest.mark.skip(reason="Performance tests not implemented yet")
+def test_memory_usage():
+    pass 
