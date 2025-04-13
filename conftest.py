fa3b337960ba8c281296a539855af6912e623387
Alex Rockwell <arockwell@gmail.com>
2025-04-13 18:26:54 -0400

    Delete conftest.py

--- conftest.py
+++
@@ -1,7 +0,0 @@
-"""Root conftest.py for pytest configuration."""
-
-import os
-import sys
-
-# Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src"))) 
