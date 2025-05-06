f76257222d5d2ed4688be0a4ae50322fc28fb061
Alex Rockwell <arockwell@gmail.com>
2025-05-06 01:53:33 -0400

    Delete add_issues_to_cli.sh

--- add_issues_to_cli.sh
+++
@@ -1,9 +0,0 @@
-#\!/bin/bash
-# Add CLI issues
-PROJECT_ID="PVT_kwHNVCzOAOC8pA"
-ISSUES=(210 209 194 82 215)
-
-for ISSUE_NUM in "${ISSUES[@]}"; do
-  echo "Adding issue #$ISSUE_NUM to CLI Improvements project"
-  gh issue edit "$ISSUE_NUM" --add-project "$PROJECT_ID"
-done
