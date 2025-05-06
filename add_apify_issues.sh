ed61bed4e3f155e07119e328035fd28be374a607
Alex Rockwell <arockwell@gmail.com>
2025-05-06 01:53:21 -0400

    Delete add_apify_issues.sh

--- add_apify_issues.sh
+++
@@ -1,8 +0,0 @@
-#\!/bin/bash
-PROJECT_NAME="Apify Integration Repository"
-ISSUES=(217 216 215 214 129 118 116 114 113 112 111 110)
-
-for ISSUE_NUM in "${ISSUES[@]}"; do
-  echo "Adding issue #$ISSUE_NUM to $PROJECT_NAME"
-  gh issue edit "$ISSUE_NUM" --add-project "$PROJECT_NAME"
-done
