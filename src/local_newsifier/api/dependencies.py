092dee8d0c651f2321138284769bb7513c0accdd
Alex Rockwell <arockwell@gmail.com>
2025-05-06 03:10:59 -0400

    Simplify get_article_service dependency (Issue #251)

--- src/local_newsifier/api/dependencies.py
+++ src/local_newsifier/api/dependencies.py
@@ -75,16 +75,7 @@ def get_article_service() -> ArticleService:
     Returns:
         ArticleService: The article service instance
     """
-    from local_newsifier.di.providers import get_article_service as get_injectable_article_service
-    from local_newsifier.database.engine import get_session
-    
-    # First try to get from injectable providers (preferred)
-    try:
-        with next(get_session()) as session:
-            return get_injectable_article_service(session=session)
-    except (ImportError, NameError):
-        # Fall back to container if injectable is not available
-        return container.get("article_service")
+    return container.get("article_service")
 
 
 def get_rss_feed_service() -> RSSFeedService:
