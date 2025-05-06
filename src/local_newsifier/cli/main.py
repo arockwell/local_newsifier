547a231f6365a06165e16014254915a0816387b2
Alex Rockwell <arockwell@gmail.com>
2025-05-06 02:20:12 -0400

    Revert "Add CLI commands and service for managing ApifySourceConfig (Issue #215)"
    This reverts commit 841b658190c9b85018abd64846b8e03c7a03c51d.

--- src/local_newsifier/cli/main.py
+++ src/local_newsifier/cli/main.py
@@ -33,29 +33,23 @@ def is_apify_command():
         bool: True if the command is apify-related
     """
     # Check if 'apify' is in the command arguments
-    return len(sys.argv) > 1 and (sys.argv[1] == "apify" or sys.argv[1] == "apify-config")
+    return len(sys.argv) > 1 and sys.argv[1] == "apify"
 
 
 # Conditionally load commands to avoid unnecessary dependencies
 if is_apify_command():
     # Only load the apify command if it's being used
-    if sys.argv[1] == "apify":
-        from local_newsifier.cli.commands.apify import apify_group
-        cli.add_command(apify_group)
-    elif sys.argv[1] == "apify-config":
-        from local_newsifier.cli.commands.apify_config import apify_config_group
-        cli.add_command(apify_config_group)
+    from local_newsifier.cli.commands.apify import apify_group
+    cli.add_command(apify_group)
 else:
     # Load all other command groups
     from local_newsifier.cli.commands.feeds import feeds_group
     from local_newsifier.cli.commands.db import db_group
     from local_newsifier.cli.commands.apify import apify_group
-    from local_newsifier.cli.commands.apify_config import apify_config_group
     
     cli.add_command(feeds_group)
     cli.add_command(db_group)
     cli.add_command(apify_group)
-    cli.add_command(apify_config_group)
 
 
 def main():
