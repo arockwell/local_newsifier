4b18456f20c85190eda5e2d4412e7a820ad246c4
Alex Rockwell <arockwell@gmail.com>
2025-05-17 03:12:14 -0400

    Remove legacy session utils

--- src/local_newsifier/database/session_utils.py
+++
@@ -1,8 +0,0 @@
-"""Legacy session utilities removed in favor of the standard provider."""
-
-# This module previously exposed ``get_container_session`` and
-# ``with_container_session`` helpers.  These have been deleted as part of the
-# migration to the ``get_session`` provider defined in
-# :mod:`local_newsifier.di.providers`.  Any code that relied on the old helpers
-# should import and use ``get_session`` directly.
-
